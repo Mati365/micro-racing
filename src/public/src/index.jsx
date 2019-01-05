6f38129ef00581791177f76a22eb51720a7017cc
Mateusz Bagiński <cziken58@gmail.com>
2019-01-05 21:40:18 +0100

    @pkg/gl-math: add package, add loop unrolling

--- src/public/src/index.jsx
+++ src/public/src/index.jsx
@@ -1,14 +1,59 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+// import ReactDOM from 'react-dom';
+// import React from 'react';
 
-import GameCanvas from './ui/GameCanvas';
+import * as M from '@pkg/gl-math';
 
-ReactDOM.render(
-  <GameCanvas
-    dimensions={{
-      width: 640,
-      height: 480,
-    }}
-  />,
-  document.getElementById('app-root'),
+// import GameCanvas from './ui/GameCanvas';
+
+const measureExec = fn => () => {
+  const t = performance.now();
+  const result = fn();
+  console.info(performance.now() - t);
+
+  return result;
+};
+
+const m1 = M.createMatrix(
+  4, 4,
+  [
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16,
+  ],
 );
+
+const m2 = M.createMatrix(
+  4, 4,
+  [
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16,
+  ],
+);
+
+console.log(M.mul4x4(m1, m2));
+measureExec(
+  () => {
+    for (let i = 1000000; i >= 0; --i)
+      M.mulMatrix(m1, m2);
+  },
+)();
+
+measureExec(
+  () => {
+    for (let i = 1000000; i >= 0; --i)
+      M.mul4x4(m1, m2);
+  },
+)();
+
+// ReactDOM.render(
+//   <GameCanvas
+//     dimensions={{
+//       width: 640,
+//       height: 480,
+//     }}
+//   />,
+//   document.getElementById('app-root'),
+// );

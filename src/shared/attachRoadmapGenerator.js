57c51772640af3bb5e592e4ac68bc457789b1df9
Mateusz Baginski <cziken58@gmail.com>
2019-03-17 11:14:55 +0100

    @pkg/convex-hull: init package

--- src/shared/attachRoadmapGenerator.js
+++ src/shared/attachRoadmapGenerator.js
@@ -1,7 +1,7 @@
 import * as R from 'ramda';
 
 import {vec2} from '@pkg/gl-math/matrix';
-import convexHull from './convexHull';
+import convexHull from '@pkg/convex-hull';
 
 /**
  * Returns random int between min and max, includes min and max
@@ -35,15 +35,17 @@ const getRandomPoint = (border, areaSize) => {
  * Creates array of points on racing map
  *
  * @param {Size} area
+ * @param {Number} randomPointsCount
+ *
  * @param {Point} border
  *
  * @returns {Array}
  */
 export const generateAreaPoints = R.curry(
-  (border, area) => {
+  (border, randomPointsCount, area) => {
     const points = R.times(
       R.partial(getRandomPoint, [border, area]),
-      20,
+      randomPointsCount,
     );
 
     return points;
@@ -54,21 +56,61 @@ export const generateAreaPoints = R.curry(
  * Renders array of points
  *
  * @param {String} color
+ * @param {Boolean} withIndices
  * @param {CanvasRenderingContext2D} ctx
+ *
+ * @param {vec2[]} points
  */
-const renderPoints = (color, withText, ctx) => R.addIndex(R.forEach)(
+const renderPoints = (color, withIndices, ctx) => R.addIndex(R.forEach)(
   ({x, y}, index) => {
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.fillStyle = color;
     ctx.fill();
 
-    if (withText) {
+    if (withIndices) {
       ctx.font = '10px Georgia';
       ctx.fillText(index, x - 2, y - 10);
     }
   },
 );
+
+/**
+ * @param {String} color
+ * @param {Number} width
+ * @param {CanvasRenderingContext2D} ctx
+ *
+ * @param {vec2[]} points
+ */
+const renderLoopedLines = (color, width, ctx) => (points) => {
+  const origin = points[0];
+
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+
+  ctx.moveTo(origin.x, origin.y);
+
+  for (let i = 1; i < points.length; i++) {
+    const {x, y} = points[i];
+    ctx.lineTo(x, y);
+  }
+
+  ctx.lineTo(origin.x, origin.y);
+  ctx.stroke();
+};
+
+/**
+ * Makes lines more curvy
+ *
+ * @param {Object} config
+ *
+ * @param {vec2[]} points
+ */
+const catmull = ({step}) => (points) => {
+  console.log(step);
+  return points;
+};
 
 /**
  * Renders map to canvas
@@ -81,12 +123,18 @@ const renderPoints = (color, withText, ctx) => R.addIndex(R.forEach)(
  */
 const attachRoadmapGenerator = (area, ref) => {
   const points = R.compose(
+    catmull(
+      {
+        step: 0.1,
+      },
+    ),
     convexHull,
     generateAreaPoints(
       {
         x: 40,
         y: 40,
       },
+      30,
     ),
   )(area);
 
@@ -95,6 +143,7 @@ const attachRoadmapGenerator = (area, ref) => {
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, area.w, area.h);
 
+  renderLoopedLines('#FFFFFF', 2, ctx)(points);
   renderPoints('#FF0000', true, ctx)(points);
 };
 
